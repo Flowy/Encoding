31ed868d42787f573c3693b44a2a4a0017dd67ca
Lukas <flowyk@gmail.com>
2015-05-09 11:41:08 +0200

    startup argument

--- src/com/flowyk/encoding/Main.java
+++ src/com/flowyk/encoding/Main.java
@@ -7,28 +7,36 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        StringBuilder inputString = new StringBuilder();
-        inputString.append("P");
-        inputString.append("OO");
-        inputString.append("NNN");
-        inputString.append("MMM");
-        inputString.append("LLL");
-        inputString.append("KKK");
-        inputString.append("JJJ");
-        inputString.append("III");
-        inputString.append("HHHH");
-        inputString.append("GGGG");
-        inputString.append("FFFFF");
-        inputString.append("EEEEE");
-        inputString.append("DDDDD");
-        inputString.append("CCCCC");
-        inputString.append("BBBBB");
-        inputString.append("AAAAA");
-        inputString.append(".....");
-        inputString.append("))))))))");
-        inputString.append("((((((((");
-        inputString.append("////////////////////");
-        CodeConstructor<String, Code> codeConstructor = new HuffmanCode<>(Arrays.asList(inputString.toString().split("")));
-        System.out.println(codeConstructor.getCodes());
+        System.out.println(Arrays.asList(args));
+        String inputString;
+        if (args.length > 0) {
+            inputString = args[0];
+        } else {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("P");
+            stringBuilder.append("OO");
+            stringBuilder.append("NNN");
+            stringBuilder.append("MMM");
+            stringBuilder.append("LLL");
+            stringBuilder.append("KKK");
+            stringBuilder.append("JJJ");
+            stringBuilder.append("III");
+            stringBuilder.append("HHHH");
+            stringBuilder.append("GGGG");
+            stringBuilder.append("FFFFF");
+            stringBuilder.append("EEEEE");
+            stringBuilder.append("DDDDD");
+            stringBuilder.append("CCCCC");
+            stringBuilder.append("BBBBB");
+            stringBuilder.append("AAAAA");
+            stringBuilder.append(".....");
+            stringBuilder.append("))))))))");
+            stringBuilder.append("((((((((");
+            stringBuilder.append("////////////////////");
+            inputString = stringBuilder.toString();
+        }
+        CodeConstructor<String, Code> codeConstructor = new HuffmanCode<>(Arrays.asList(inputString.split("")));
+        System.out.println("Vstupny retazec: " + inputString);
+        System.out.println("Vygenerovane kody podla huffmanovho kodovania: " + codeConstructor.getCodes());
     }
 }
